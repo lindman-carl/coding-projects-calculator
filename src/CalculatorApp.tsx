76f1ea5ac06df5f649f2d08a0237192138acc8b4
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-14 01:50:17 +0200

    add aria to display

--- src/CalculatorApp.tsx
+++ src/CalculatorApp.tsx
@@ -83,6 +83,7 @@ const CalculatorApp = () => {
             }
           }}
           autoFocus
+          aria-label="calculator display"
         />
       </form>
       {/* Calculator buttons */}
