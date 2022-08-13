e2a773eb910bccec7c596f82ea6d91ac14ea9576
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-13 14:37:00 +0200

    add basic calculator layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import CalculatorButton from "./components/CalculatorButton";
 
+const buttonLabels = [
+  "7",
+  "8",
+  "9",
+  "/",
+  "4",
+  "5",
+  "6",
+  "x",
+  "1",
+  "2",
+  "3",
+  "-",
+  "0",
+  ",",
+  "=",
+  "+",
+];
+
 function App() {
+  const [calculatorResults, setCalculatorResults] = useState<number>(1237474);
+  const [showResults, setShowResults] = useState<boolean>(false);
+  const [calculatorInstructions, setCalculatorInstructions] =
+    useState<string>("hej");
+
   return (
-    <div className="bg-blue-200">
-      <CalculatorButton label="+" />
+    <div className="flex flex-col justify-center items-center m-4">
+      <div className="grid grid-cols-4 grid-flow-row gap-1">
+        <div className="calculator-display col-span-4">
+          {showResults ? calculatorResults : calculatorInstructions}
+        </div>
+        {buttonLabels.map((label) => (
+          <CalculatorButton label={label} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/CalculatorButton.tsx
+++ src/components/CalculatorButton.tsx
@@ -4,7 +4,16 @@ type Props = {
   label: string;
 };
 
+const isNumber = (value: string) => {
+  return /^-?\d+$/.test(value);
+};
+
 const CalculatorButton = ({ label }: Props) => {
+  // check for special character
+  if (!isNumber(label)) {
+    return <div className="calculator-button bg-orange-600">{label}</div>;
+  }
+
   return <div className="calculator-button">{label}</div>;
 };
 
