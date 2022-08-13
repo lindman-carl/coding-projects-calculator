f0ab894a277b7acb4411a39649d09a7d7ab10010
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-13 17:24:20 +0200

    add basic tests

--- src/CalculatorApp.test.tsx
+++ src/CalculatorApp.test.tsx
@@ -2,8 +2,38 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import CalculatorApp from "./CalculatorApp";
 
-test("renders learn react link", () => {
+// utils
+import {
+  buttonLabels,
+  evaluateCalculatorInstructions,
+} from "./utils/calculator";
+
+test("CalculatorApp loads and renders all buttons", () => {
   render(<CalculatorApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElements = buttonLabels.map((label) => screen.getByText(label));
+
+  expect(buttonElements.length).toBe(buttonLabels.length);
+});
+
+test("Evaluation of invalid expression returns error message", () => {
+  const [results, errorMsg] = evaluateCalculatorInstructions("92-asd");
+
+  expect(results).toBe("");
+  expect(errorMsg).not.toBeNull();
+});
+
+test("Evaluation of valid expressions return expected results", () => {
+  const calculations = [
+    ["4+5*(12/3)", "24"],
+    ["2+4", "6"],
+    ["5*5", "25"],
+    ["4/4", "1"],
+    ["17*2+4", "38"],
+    ["(9999-20)*2", "19958"],
+  ];
+
+  for (let [expression, expectedResult] of calculations) {
+    const [result] = evaluateCalculatorInstructions(expression);
+    expect(result).toBe(expectedResult);
+  }
 });
