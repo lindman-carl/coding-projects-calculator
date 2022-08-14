43062696608611a6415e3bc45bbe55a0cb83193d
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-14 16:13:46 +0200

    refactored ClipboardButton component

--- src/components/CalculatorDisplay.tsx
+++ src/components/CalculatorDisplay.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-import { TbClipboardText } from "react-icons/tb";
-import { toast, ToastOptions } from "react-toastify";
+import ClipboardButton from "./ClipboardButton";
 
 // avoid prop drilling
 type Props = {
@@ -10,60 +9,6 @@ type Props = {
   setCalculatorInstructions: React.Dispatch<React.SetStateAction<string>>;
   getInputValue: () => string;
   calculatorError: string;
-};
-
-const copyToClipboard = async (text: string) => {
-  await navigator.clipboard.writeText(text);
-};
-
-type ClipboardButtonProps = {
-  value: string;
-};
-
-const basicToast: ToastOptions = {
-  position: "bottom-right",
-  autoClose: 2000,
-  hideProgressBar: true,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-  progress: undefined,
-};
-
-const ClipboardButton = ({ value }: ClipboardButtonProps) => {
-  const copyCalculatorInstructionsToClipboard = async () => {
-    if (value.length > 0) {
-      // copy to clipboard
-      copyToClipboard(value);
-      toast.info(`${value} copied to clipboard`, {
-        ...basicToast,
-      });
-
-      return;
-    }
-
-    toast(`Do math first`, basicToast);
-  };
-
-  return (
-    <div
-      className="w-24 h-10
-        rounded-br-md
-        text-sky-100 dark:text-purple-100
-        bg-sky-300 dark:bg-purple-300
-        bg-opacity-20 dark:bg-opacity-20
-        hover:scale-95 origin-top-left
-        transition-transform duration-100 ease-in-out
-        cursor-pointer
-        shadow
-     "
-      aria-label="Copy to clipboard"
-      title="Copy to clipboard (Ctrl+C)"
-      onClick={copyCalculatorInstructionsToClipboard}
-    >
-      <TbClipboardText className="w-full h-full p-2" />
-    </div>
-  );
 };
 
 const CalculatorDisplay = ({
---
+++ src/components/ClipboardButton.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+// icon
+import { TbClipboardText } from "react-icons/tb";
+import { ToastOptions } from "react-toastify";
+
+const copyToClipboard = async (text: string) => {
+  await navigator.clipboard.writeText(text);
+};
+
+const basicToast: ToastOptions = {
+  position: "bottom-right",
+  autoClose: 2000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
+
+type ClipboardButtonProps = {
+  value: string;
+};
+
+const ClipboardButton = ({ value }: ClipboardButtonProps) => {
+  const copyCalculatorInstructionsToClipboard = async () => {
+    if (value.length > 0) {
+      // copy to clipboard
+      copyToClipboard(value);
+      toast.info(`${value} copied to clipboard`, {
+        ...basicToast,
+      });
+
+      return;
+    }
+
+    toast.warn(`Do math first`, basicToast);
+  };
+
+  return (
+    <div
+      className="clipboard-button"
+      aria-label="Copy to clipboard"
+      title="Copy to clipboard (Ctrl+C)"
+      onClick={copyCalculatorInstructionsToClipboard}
+    >
+      <TbClipboardText className="w-full h-full p-2" />
+    </div>
+  );
+};
+export default ClipboardButton;
