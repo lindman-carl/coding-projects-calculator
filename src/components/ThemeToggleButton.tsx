26d558bfd3a9e1a71de2e0bd9d35331653f1d2ca
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-08-14 15:51:28 +0200

    refactored themeToggleButton style

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -8,14 +8,14 @@ const ThemeToggleButton = () => {
   const { theme, setTheme } = useThemeContext();
 
   return (
-    <div className="w-10 h-10 flex justify-center items-center transition duration-100 ease-in-out rounded-lg p-2 border-2 border-sky-200 dark:border-purple-500 bg-sky-500 dark:bg-purple-800">
+    <div className="theme-toggle-container">
       {theme === "dark" ? (
         <FaRegSun
           aria-label="dark mode icon"
           data-testid="darkModeIcon"
           size="2rem"
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="text-purple-100 text-2xl cursor-pointer"
+          className="text-purple-100"
         />
       ) : (
         <FaRegMoon
@@ -23,7 +23,7 @@ const ThemeToggleButton = () => {
           data-testid="darkModeIcon"
           size="2rem"
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="text-sky-100 text-2xl cursor-pointer"
+          className="text-sky-100"
         />
       )}
     </div>
